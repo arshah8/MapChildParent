ce1ea5b276609de7bae7f56d9521747aa1ebfa05
Abdur Rahman Shah <dragema44@gmail.com>
2025-01-02 01:49:18 +0500

    error solved

--- src/app/Components/Child.tsx
+++ src/app/Components/Child.tsx
@@ -1,16 +1,16 @@
 
 
-export default function Child({ carImage, carName, carPrice, carReviews }) {
+export default function Child(props) {
   return (
     <div className="border rounded-lg p-4 bg-white shadow-lg">
       <img
-        src={carImage}
-        alt={carName}
+        src={props.carImage}
+        alt={props.carName}
         className="w-full h-30 object-cover rounded-t-lg"
       />
-      <h3 className="text-lg text-gray-600 font-bold mt-2">{carName}</h3>
-      <p className="text-gray-500">{carPrice}</p>
-      <p className="text-sm text-yellow-500">{carReviews}</p>
+      <h3 className="text-lg text-gray-600 font-bold mt-2">{props.carName}</h3>
+      <p className="text-gray-500">{props.carPrice}</p>
+      <p className="text-sm text-yellow-500">{props.carReviews}</p>
     </div>
   );
 }
